ccc4ab05b141b640f3148fd576dd09fcafbbf76a
Kauanne Oliveira <paulakauanne2411@gmail.com>
2026-03-09 11:14:16 -0300

    comeco do LoginView.kt

--- app/src/main/java/com/example/carteirinhadigital/LoginView.kt
+++ app/src/main/java/com/example/carteirinhadigital/LoginView.kt
@@ -1,24 +1,38 @@
 package com.example.carteirinhadigital
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose.CarteirinhaDigitalTheme
 
 @Composable
 fun LoginView(
     modifier: Modifier = Modifier
 ) {
-    Column() {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(
+            space = 16.dp,
+            alignment = Alignment.CenterVertically
+        )
+    ) {
         TextField(
             value = "",
             onValueChange = {},
@@ -34,8 +48,20 @@ fun LoginView(
                 Text("Login")
             },
         )
+
         Button(
-            onClick = {}
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth(.8f),
+            shape = RoundedCornerShape(size = 5.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.background,
+
+            ),
+            border = BorderStroke(
+                2.dp,
+                MaterialTheme.colorScheme.primary
+            )
         ) {
             Text("Entrar")
         }
@@ -48,18 +74,32 @@ fun LoginView(
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun PreviewLoginClaro(){
     CarteirinhaDigitalTheme(darkTheme = false){
-        LoginView()
+        LoginView(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        )
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun PreviewLoginEscuro(){
     CarteirinhaDigitalTheme(darkTheme = true){
-        LoginView()
+        LoginView(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        )
     }
 }
--- app/src/main/java/com/example/carteirinhadigital/core/designsystem/theme/ValueText.kt
+++ app/src/main/java/com/example/carteirinhadigital/core/designsystem/theme/ValueText.kt
@@ -1,4 +1,4 @@
-package com.example.carteirinhadigital.ui.theme
+package com.example.carteirinhadigital.core.designsystem.theme
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
