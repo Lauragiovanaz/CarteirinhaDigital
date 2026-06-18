758f40d21e787106e4c5aa83bd475cf255ff3801
Kauanne Oliveira <paulakauanne2411@gmail.com>
2026-06-18 19:09:42 -0300

    feat(auth): implementar FakeAuthRepository com credenciais e delay

---
+++ app/src/main/java/com/example/carteirinhadigital/feature/auth/data/repository/FakeAuthRepository.kt
@@ -0,0 +1,19 @@
+package com.example.carteirinhadigital.feature.auth.data.repository
+
+import com.example.carteirinhadigital.feature.auth.domain.model.UsuarioLogado
+import kotlinx.coroutines.delay
+
+class FakeAuthRepository : AuthRepository {
+    override suspend fun login(usuario: String, senha: String): UsuarioLogado? {
+        delay(1500)
+        return if (usuario == "aluno" && senha == "aluno1234") {
+            UsuarioLogado(
+                id = "001",
+                nome = "Aluno SENAI",
+                token = "fake-token-senai-2024"
+            )
+        } else {
+            null
+        }
+    }
+}
